672a04c577995acf8d493060e8a3474b708bdaa1
unknown <ademodiseun@gmail.com>
2021-04-20 10:16:41 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace CommonOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // +, /, -, %, *    Arithmetics operators
+            //  int numerator = 10;
+            //  int denominator = 3;
+            // int result = numerator % denominator;
+            // Console.WriteLine(result);
+
+        // string name = "ifedayo";
+        // string password = "123";
+        // string nameinput = Console.ReadLine();
+        // string passwordinput =  Console.ReadLine();
+        // if (!(name == nameinput))
+        // {
+        //     Console.WriteLine( "true");
+        // }
+        //   int x = 20;
+        //   int y = 10;
+        //   if (x>y)
+        //   {
+        //       Console.WriteLine("correct");
+        //   }
+        //   else
+        //   {
+        //       Console.WriteLine("not correct");
+        //   }
+
+        //   int x = 20;
+        //   int y = 10;
+        //  // ternary operators
+        //  string result = x>y ? "x is greater than y" : "x is less than y";
+        //  Console.WriteLine(result);
+
+        //    int a = 1;
+        //    a++;
+        //    Console.WriteLine(a);
+
+        int a = 40;
+       a--;
+      Console.WriteLine(a);
+        }
+    }
+}
